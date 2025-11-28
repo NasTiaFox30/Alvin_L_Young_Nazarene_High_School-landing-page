f650b05ce6a2aeba0dd6351fe13b5100239ca46f
Anastasiia Bzova <bzovanastia@gmail.com>
2025-11-28 21:57:49 +0100

    Update names in package json files

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: /name /           //Replace
+  base: /Alvin_L_Young_Nazarene_High_School-landing-page/           //Replace
 })
